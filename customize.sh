a537fe406c049375fa5c10c58b1665f9966eb5c9
kmiit <2421768138@qq.com>
2023-02-21 08:31:29 +0800

    Update some print information

--- customize.sh
+++ customize.sh
@@ -11,27 +11,31 @@ else
 SLOT="_b"
 fi 
 fi
+ui_print "Fetching boot.img"
 dd if=/dev/block/by-name/boot$SLOT of=./boot.img
 ./magiskboot unpack boot.img
+ui_print "Patching boot.img"
 mv ./Image ./kernel
 ./magiskboot repack boot.img new.img
+ui_print "Done"
 }
 
 FLASHBOOT(){
+ui_print "Flashing new boot to boot$SLOT"
 dd if=./new.img of=/dev/block/by-name/boot$SLOT
-ui_print "Done."
 }
 
 TMPDIR=/data/local/tmp_ksu_install
 mkdir $TMPDIR 2>/dev/null
-ui_print "extracting fies..."
+ui_print "Extracting fies..."
 unzip "$ZIPFILE" -d "$TMPDIR"
 cd $TMPDIR
+ui_print "Setting up environment"
 chmod 755 magiskboot
 chmod 755 bootctl
+ui_print "Done"
 
 SLOT_COUNT=`./bootctl get-number-slots`
-ui_print "$SLOT_COUNT"
 case $SLOT_COUNT in
 [1])
 PATCHBOOT
