7aa0b8ed579b19cccebd21dd3837eee370a0fc27
kmiit <2421768138@qq.com>
2023-02-22 14:12:31 +0800

    support ota,add backup/restore

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
 # Install-Update-Ksu-kernel-via-Ksu-manager
-This module allows you to flash ksu kernel just in ksu manager.
+ - This module allows you to flash ksu kernel just in ksu manager./
+ - 可以使用本模块在ksu管理器里对boot进行刷写
+ 
+# Support
+ - Backup&Restore boot / 备份/恢复boot
+ - Flash target boot slot after OTA / 刷入ota后的槽位（重启前）
 
 # How to use
  - place the ksu kernel in the root dir of this module
  - and rename it to Image
- - zip it and flash in ksu manager
-
+ - if you want to restore boot, donnot place any file
+ - named Image in this folder
+ - zip the whole folder and flash in ksu manager
+//把ksu内核命名为 Image 并放入模块根目录（如果是恢复boot则不要放进来）
+用zip压缩模块并在ksu管理器刷入
 
 
 # Tools
  - magiskboot,bootctl 
  - all extracted from Magisk
-
+ - 工具提取自magisk
 
 # 注意
  - 不一定适合所有设备
- - 目前只在Redmi k60pro （gki2.0 5.15）测试成功
- - 暂时没有测试ota后运行情况（预计刷不进去）
- - 请做好备份，出现事故概不负责
+ - 目前只在Redmi k60pro （gki2.0 5.15 vab）测试成功
+ - OTA后刷入目前测试成功一次
+ - ota后备份boot只会保留本次刷写前对应分区的boot
+ - ota后请只刷入一次ksu内核，否则可能破坏备份的官方boot
+ - 恢复boot暂未测试，也许会失败
+ - 请手动做好备份，出现事故概不负责
+
+ - 正在优化判断逻辑，八百年不一定更新一次
--- customize.sh
+++ customize.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 SKIPUNZIP=1
 
+BACKUPBOOT(){
+if [ $ifota -eq 1 ] || [ ! -e "/data/adb/stock_boot.img" ]; then
+ui_print "Backup boot..."
+dd if=$BOOTPATH of=/data/adb/stock_boot.img
+ui_print "Done"
+else
+ui_print "Not backup boot"
+fi
+}
+
+RESTOREBOOT(){
+if [ ! -e "$TMPDIR/Image" ] && [ -e "/data/adb/stock_boot.img" ];then
+ui_print "Restore boot..."
+dd if=/data/adb/stock_boot.img of=$BOOTPATH
+ui_print "Done"
+fi
+rm -rf $TMPDIR
+exit
+}
+
 PATCHBOOT(){
 if [ -z $1 ]; then
 SLOT=""
-else
-if [ $1 -eq 0]; then 
+elif [ $1 -eq 0 ]; then 
 SLOT="_a"
-else
+elif [ $1 -eq 1 ]; then
 SLOT="_b"
-fi 
 fi
+BOOTPATH=/dev/block/by-name/boot$SLOT
+blockdev --setrw $BOOTPATH 2>/dev/null
+RESTOREBOOT
+BACKUPBOOT
 ui_print "Fetching boot.img"
-dd if=/dev/block/by-name/boot$SLOT of=./boot.img
+dd if=$BOOTPATH of=./boot.img
 ./magiskboot unpack boot.img
 ui_print "Patching boot.img"
 mv ./Image ./kernel
@@ -20,17 +42,29 @@ mv ./Image ./kernel
 ui_print "Done"
 }
 
+CHECKOTA(){
+current_slot=`./bootctl get-current-slot`
+target_slot=`./bootctl get-active-boot-slot`
+if [ $current_slot -eq $target_slot ]; then
+ifota=0
+else
+ifota=1
+fi
+ui_print "Your device has OTAed (if you had not change the boot slot by yourself)"
+}
+
 FLASHBOOT(){
 ui_print "Flashing new boot to boot$SLOT"
 dd if=./new.img of=/dev/block/by-name/boot$SLOT
 }
 
+MAIN(){
+ui_print "$MODPATH,$ZIPFILE"
+ui_print "Setting up environment"
 TMPDIR=/data/local/tmp_ksu_install
 mkdir $TMPDIR 2>/dev/null
-ui_print "Extracting fies..."
 unzip "$ZIPFILE" -d "$TMPDIR"
 cd $TMPDIR
-ui_print "Setting up environment"
 chmod 755 magiskboot
 chmod 755 bootctl
 ui_print "Done"
@@ -42,12 +76,13 @@ PATCHBOOT
 FLASHBOOT
 ;;
 [2])
-ACTIVESLOT=`./bootctl get-active-boot-slot`
-PATCHBOOT $ACTIVESLOT
+CHECKOTA
+PATCHBOOT $target_slot
 FLASHBOOT
 ;;
 *)
 ui_print "unknown device slots"
 esac
+}
 
-rm -rf $TMPDIR
+MAIN
